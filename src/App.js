dca4b349aa4336bb770e499fc0457a59cd727fba
Sungyeon Yoo <47223114+sungyeonu@users.noreply.github.com>
2022-05-22 00:53:14 -0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import AppBar from './components/Navbar/AppBar';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <AppBar />
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
---
+++ src/components/Navbar/NavItems.js
@@ -0,0 +1,6 @@
+export const NAV_ITEMS = [
+  {
+    label: 'ZECMarketCap',
+    href: '#',
+  },
+];
