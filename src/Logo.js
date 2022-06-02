e11062d7c4d2b76eb79da2efd152f180e6a19a17
Sungyeon Yoo <47223114+sungyeonu@users.noreply.github.com>
2022-06-01 22:59:01 -0400

    update icon

--- src/Logo.js
+++ src/Logo.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Image } from '@chakra-ui/react';
-import logo from './zcashLogo.png';
-
+import lightLogo from './zmc_black.png';
+import darkLogo from './zmc_white.png';
+import { useColorModeValue } from '@chakra-ui/react';
 export const Logo = props => {
+  const logo = useColorModeValue(lightLogo, darkLogo);
+
   return <Image src={logo} {...props} />;
 };
